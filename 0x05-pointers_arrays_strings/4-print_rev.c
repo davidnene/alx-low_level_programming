4a9bfb18ba3f58fbb67052b60544693f3b801902
root <root@DESKTOP-A6PAFO0.localdomain>
2022-03-21 22:40:19 +0300

    Add 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a sreing in reverse
+ * followed by a new line
+ * @s: a pointer to a string
+ * Return: void meaning return nothing
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while(len)
+		_putchar(s[--len]);
+
+	_putchar('\n');
+}

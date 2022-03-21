39344f31b5b4751ef416ec13bf2bc34177642c57
root <root@DESKTOP-A6PAFO0.localdomain>
2022-03-21 20:45:09 +0300

    Add 0-reset_to_98.c file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates an integer value to 98
+ * @n: A pointer to an int that will be updated
+ *
+ * Return: void meaning nothing to return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

d141062a296b0ed327d4f494a7f5201521f56ad6
root <root@DESKTOP-A6PAFO0.localdomain>
2022-04-01 13:28:30 +0300

    Add 1-args.c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints out number of arguements passed to it
+ * @argc: counts the number of arguements
+ * @argv: prints the arguments in a commandline
+ * Return: zero
+ */
+
+int __attribute__ ((unused)) main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}

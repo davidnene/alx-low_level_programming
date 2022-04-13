3a9b12ae2610e0dbc36068bae1f9d7e1e482f51e
DavidNene <davienesh4@gmail.com>
2022-04-13 23:58:13 +0300

    Add file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,28 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-memset.c                                   /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include<stddef.h>
+/**
+ * print_name - a function ...
+ * @name: the chaine
+ * @f: the function
+ *
+ * Return: 1 or 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+	(*f)(name);
+}
+

f5460e11645dd303183a9f70c0d90393dcfdc185
DavidNene <davienesh4@gmail.com>
2022-04-26 12:47:01 +0300

    Fix lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <string.h>
+
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -31,4 +33,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
 #endif
